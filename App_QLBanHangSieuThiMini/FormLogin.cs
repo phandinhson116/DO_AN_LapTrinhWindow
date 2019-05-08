8178ad0c4e0da0af55830417abf6d495721487db
Phan Đình Sơn <39423369+phandinhson116@users.noreply.github.com>
2019-05-08 22:47:23 +0700

    Thay đổi giao diện Login Kết nối vs cơ sở dữ liệu

--- App_QLBanHangSieuThiMini/FormLogin.cs
+++ App_QLBanHangSieuThiMini/FormLogin.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,10 @@ namespace App_QLBanHangSieuThiMini
 {
     public partial class FormLogin : Form
     {
+        string ConnStr= "Data Source=DESKTOP-LRIE6A1;Initial Catalog=QL_BanHang;Integrated Security=True";
+        SqlConnection conn = null;
+        SqlCommand cmd = null;
+        SqlDataReader data = null;
         public FormLogin()
         {
             InitializeComponent();
@@ -60,10 +65,55 @@ namespace App_QLBanHangSieuThiMini
         }
         #endregion
 
-        private void btnLogin_Click(object sender, EventArgs e)
+       
+
+        private void FormLogin_Load(object sender, EventArgs e)
         {
-            Form fManager = new FormManager();
-            fManager.ShowDialog();
+            this.gbDangNhap.BackColor = System.Drawing.Color.Transparent;
+        }
+
+        private void btnLogin_Click_1(object sender, EventArgs e)
+        {
+            conn = new SqlConnection(ConnStr);
+            try
+            {
+                conn.Open();
+                string account = txtAccount.Text;
+                string pass = txtPassword.Text;
+
+                string sql= "Select MaNV,MatKhau From NhanVien where MaNV='" + account + "'and MatKhau='" + pass + "'";
+                cmd = new SqlCommand(sql, conn);
+                data = cmd.ExecuteReader();
+                if (data.Read() == true)
+                {
+                    Form fManager = new FormManager();
+                    fManager.ShowDialog();
+                }
+                else
+                {
+                    MessageBox.Show("Không đúng tên người dùng / mật khẩu !!!",
+                    "Thông báo");
+                    txtAccount.Focus();
+                }
+
+            }
+            catch (SqlException ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+           
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn có muốn thoát không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button2, MessageBoxOptions.ServiceNotification);
+            if (result == DialogResult.Yes)
+            {
+                this.Close();
+            }
+          
         }
     }
 }
